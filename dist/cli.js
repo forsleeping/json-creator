615301d3b6f050da62015d019ea3aad48f750f5b
WANG SHAOYU <wsysuper@gmail.com>
2019-07-09 18:52:26 +0900

    add readme (#1)

--- dist/cli.js
+++ dist/cli.js
@@ -1,21 +1,15 @@
 #! /usr/bin/env node
-// example:
-// $ echo '[{"3..5": "${RANGE(_.a, _.b)}"}]' | json-creator -d '{a:10, b:15}'
-// [[10,11,12,13,14],[10,11,12,13,14],[10,11,12,13,14],[10,11,12,13,14],[10,11,12,13,14]]
-// $ echo '[{"3..5": "${FAKE(`{{hacker.phrase}}`)}"}]' | json-creator -i 4
-// [
-//     "If we compress the bandwidth, we can get to the PNG sensor through the optical IB program!",
-//     "I'll navigate the bluetooth CSS capacitor, that should card the RSS program!",
-//     "You can't transmit the program without overriding the solid state JBOD transmitter!",
-//     "The PNG bus is down, compress the primary monitor so we can transmit the HDD protocol!"
-// ]
 "use strict";
 
 var _json = _interopRequireDefault(require("json5"));
 
 var _commander = _interopRequireDefault(require("commander"));
 
-var _index = _interopRequireDefault(require("./index"));
+var _index = _interopRequireWildcard(require("./index"));
+
+var _package = require("../package.json");
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj["default"] = obj; return newObj; } }
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
@@ -27,7 +21,14 @@ function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = 
 
 function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
-_commander["default"].version('0.0.1').option('-d, --data-context [object]', 'data context', _json["default"].parse).option('-m, --method-context [object]', 'method context', _json["default"].parse).option('-n, --data-namespace [namespace]', 'data namespace, default to "_"').option('-g, --method-namespace [namespace]', 'data namespace, default to global context').option('-i, --json-indent [indent]', 'output json indent number', parseInt, 0).parse(process.argv);
+_commander["default"].version(_package.version).option('-d, --data-context [object]', 'data context', _json["default"].parse).option('-m, --method-context [object]', 'method context', _json["default"].parse).option('-n, --data-namespace [namespace]', 'data namespace, default to "_"').option('-g, --method-namespace [namespace]', 'data namespace, default to global context').option('-i, --json-indent [indent]', 'output json indent number', parseInt, 0).option('-l, --list', 'output available util functions').parse(process.argv);
+
+if (_commander["default"].list) {
+  Object.keys(_index.utilFunctions).forEach(function (funcName) {
+    return console.log(funcName);
+  });
+  process.exit(0);
+}
 
 var inputText = '';
 process.stdin.on('data', function (chunk) {
--- src/cli.js
+++ src/cli.js
@@ -1,23 +1,12 @@
 #! /usr/bin/env node
 
-// example:
-// $ echo '[{"3..5": "${RANGE(_.a, _.b)}"}]' | json-creator -d '{a:10, b:15}'
-// [[10,11,12,13,14],[10,11,12,13,14],[10,11,12,13,14],[10,11,12,13,14],[10,11,12,13,14]]
-
-// $ echo '[{"3..5": "${FAKE(`{{hacker.phrase}}`)}"}]' | json-creator -i 4
-// [
-//     "If we compress the bandwidth, we can get to the PNG sensor through the optical IB program!",
-//     "I'll navigate the bluetooth CSS capacitor, that should card the RSS program!",
-//     "You can't transmit the program without overriding the solid state JBOD transmitter!",
-//     "The PNG bus is down, compress the primary monitor so we can transmit the HDD protocol!"
-// ]
-
 import JSON5 from 'json5';
 import program from 'commander';
-import jsonCreator from './index';
+import jsonCreator, { utilFunctions } from './index';
+import { version } from '../package.json';
 
 program
-    .version('0.0.2')
+    .version(version)
     .option('-d, --data-context [object]', 'data context', JSON5.parse)
     .option('-m, --method-context [object]', 'method context', JSON5.parse)
     .option(
@@ -34,7 +23,13 @@ program
         parseInt,
         0
     )
+    .option('-l, --list', 'output available util functions')
     .parse(process.argv);
+
+if (program.list) {
+    Object.keys(utilFunctions).forEach(funcName => console.log(funcName));
+    process.exit(0);
+}
 
 let inputText = '';
 process.stdin.on('data', chunk => {
